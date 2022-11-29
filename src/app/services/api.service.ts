498535cfeaddf35e5ecfe089d30971ac0f499b9a
Gabriele Roncolato <gabriele.roncolato@studenti.univr.it>
2022-11-29 10:13:55 +0100

    MultiTypeCoCoSocket
    Implemented MultiTypeCommand and MultiTypeCoCoSocket which accept multiple types as responses from the websocket.
    Implemented the connect api function and Connect command which accept LobbyMatchJoined and LobbyUpdate type responses.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -42,22 +42,23 @@ export class ApiService {
         }
       }
     }
+
     cmdNewGame.resultError = error;
     cmdNewGame.run();
     return cmdNewGame;
   }
 
-  public connect( result:(lobbyData:Packets.Result<Packets.MatchInfo, string>) => void, 
-  result2:(lobbyData:Packets.MatchInfo) => void,
+  public connect( result:(lobbyData:Packets.Reply.LobbyJoinedMatch) => void, 
+  result2:(lobbyData:Packets.Reply.LobbyUpdate) => void,
   lobby_id:string, lobby_name:string, password?:string, error?:(error:string)=>void){
     
     let cmdConnect = new Commands.Connect(this.url, lobby_id, lobby_name, password);
+    
     cmdConnect.lobbyJoined = (message) => {
-      if(result) {result(message.info)}
+      if(result) {result(message)}
     }
     cmdConnect.lobbyUpdated = (message) => {
-      if(result2) {result2(message.info)}
-      console.log(message);
+      if(result2) {result2(message)}
     }
     cmdConnect.resultError = error;
     cmdConnect.run();
@@ -84,13 +85,8 @@ export class CoCoSocket{
     this.ws.subscribe({
       error: (err) => { // Called whenever there is a message from the server.
         let errorMsg = JSON.stringify(err);
-        alert(errorMsg);
         if (this.resultError) {this.resultError(errorMsg );}
-      }
-    });
-    
-
-    this.ws.subscribe({
+      },
       complete: () => {
         if (this.resultClosed) {this.resultClosed();}
       }
@@ -103,6 +99,7 @@ export class CoCoSocket{
     request:Packets.Request.Message, 
     reply: new ()=>T, 
     recieved?:(message:T)=>void){
+      
     if (this.ws == null) {return false}
     
     /*
@@ -119,6 +116,71 @@ export class CoCoSocket{
         let msg = new reply();
         let success = msg.fromPacket(payload);
         if (success && recieved){ recieved(msg); }
+      } 
+    });
+
+
+    let packet = request.toPacket();
+    this.ws.next(packet);
+
+    return this.ws;
+  }
+}
+
+export class MultiTypeCoCoSocket{
+  public url = 'ws://localhost:8088';
+  public ws?:WebSocketSubject<string>;
+  public resultError?:(error:string)=>void;
+  public resultClosed?:()=>void;
+
+  public msgClasses = Array<string>();
+  
+  constructor(url:string){
+    this.url = url;
+  }
+
+  public connect():boolean{
+
+    if (!this.ws || this.ws.closed ){
+      this.ws = new WebSocketSubject(this.url);
+    }
+    
+    this.ws.subscribe({
+      error: (err) => { // Called whenever there is a message from the server.
+        let errorMsg = JSON.stringify(err);
+        if (this.resultError) {this.resultError(errorMsg );}
+      },
+      complete: () => {
+        if (this.resultClosed) {this.resultClosed();}
+      }
+    });
+    
+    return !this.ws.closed;
+  }
+  
+  public send<T extends Packets.MultiTypeMessage>(
+    request: Packets.Request.Message, 
+    reply: new (msgClasses: Array<string>)=>T, 
+    recieved?:(message:T, messageName: string) => void,
+    ...msgClasses: Array<string>){
+      
+    if (this.ws == null) {return false}
+    
+    /*
+    let messageType = request.messageName()
+    let messageObserver = this.ws.multiplex(
+      ()=>({subscribe: messageType}),
+      ()=>({unsubscribe: messageType}),
+      (message:any) => message.type === messageType
+    )
+    */
+
+    this.ws.subscribe({
+      next: (payload) => { // Called whenever there is a message from the server.
+        let msg = new reply(msgClasses);
+        let messageName = msg.fromPacketStr(payload);
+        
+        if (messageName !== "" && recieved){ recieved(msg, messageName); }
       } 
     });
 
@@ -150,7 +212,48 @@ export namespace Commands{
       this.ws.connect();
 
       let msg = new Packets.Request.Handshake();
-      this.ws.send(msg, Packets.Reply.Handshake, (message)=>{this.handshakeRecieved(message)} );
+      this.ws.send(msg, Packets.Reply.Handshake, 
+        (message)=>{this.handshakeRecieved(message)} );
+    }
+
+    public connectionClosed(){
+
+      //alert("Command:connectionClosed");
+      if (this.resultClosed){ this.resultClosed();}
+    }
+
+    public connectionError(error:any){
+      //alert("Command:connectionError "+error);
+      if (this.resultError){ this.resultError(error);}
+    }
+
+    public handshakeRecieved(message:Packets.Reply.Handshake){
+      //alert("Command:handshakeRecieved");
+      if (this.resultHandshake && message) { this.resultHandshake(message); }
+    }
+  }
+
+  export class MultiTypeCommand{
+    public ws?: MultiTypeCoCoSocket;
+    public url?:string; 
+    public resultHandshake?:(message:Packets.Reply.Handshake)=>void; 
+    public resultClosed?:()=>void;
+    public resultError?:(error:any)=>void;
+
+    constructor(url:string){
+      this.url = url;
+    }
+
+    public run(){
+      this.ws = new MultiTypeCoCoSocket(this.url!);
+      this.ws.resultError = (error)=>{ this.connectionError(error); };
+      this.ws.resultClosed = ()=>{ this.connectionClosed(); };
+      this.ws.connect();
+
+      let msg = new Packets.Request.Handshake();
+      this.ws.send(msg, Packets.Reply.Handshake, (message, messageName)=>{
+        this.handshakeRecieved(message)
+      }, Packets.Reply.Handshake.name);
     }
 
     public connectionClosed(){
@@ -207,10 +310,9 @@ export namespace Commands{
 
       this.ws!.send(this.msg!, Packets.Reply.GameNew, (message)=>{ if(this.newLobbyCreated && message) this.newLobbyCreated(message) });
     }
-
   }
 
-  export class Connect extends Command{
+  export class Connect extends MultiTypeCommand{
     public lobbyJoined?:(message:Packets.Reply.LobbyJoinedMatch )=>void;
     public lobbyUpdated?:(message:Packets.Reply.LobbyUpdate)=>void;
     private msg?:Packets.Request.LobbyJoinMatch;
@@ -221,15 +323,17 @@ export namespace Commands{
       this.msg = new Packets.Request.LobbyJoinMatch(lobby_id, player_name, lobby_password);
     }
 
-    public override handshakeRecieved( message: Packets.Reply.Handshake){
+    public override handshakeRecieved(message: Packets.Reply.Handshake){
       super.handshakeRecieved(message);
-      this.ws!.send(this.msg!, Packets.Reply.LobbyJoinedMatch || Packets.Reply.LobbyUpdate, (message)=>{ 
-        if(this.lobbyJoined && message && message.messageName() === "LobbyJoinedMatch")
+
+      this.ws!.send(this.msg!, Packets.Reply.ConnectReply,
+        (message, messageName)=>{ 
+        
+        if(this.lobbyJoined && message && messageName === Packets.Reply.LobbyJoinedMatch.name)
           this.lobbyJoined(message as Packets.Reply.LobbyJoinedMatch);
-        else if(this.lobbyUpdated && message && message.messageName() === "LobbyUpdated")
+        else if(this.lobbyUpdated && message && messageName === Packets.Reply.LobbyUpdate.name)
           this.lobbyUpdated(message as Packets.Reply.LobbyUpdate);
-        alert("recv with " + message);
-      });
+      }, Packets.Reply.LobbyJoinedMatch.name, Packets.Reply.LobbyUpdate.name);
     }
   }
 }
@@ -242,7 +346,7 @@ export namespace Packets{
       return this.constructor.name;
     }
 
-    public toPacket():any{
+    public toPacket(): any{
       const packetName = this.messageName();
       const packet = { [packetName]:this };
       return packet;
@@ -276,6 +380,54 @@ export namespace Packets{
       const msg = new cls();
       if (msg.fromPacket(payload) === false ) {return null;}
       return msg;
+    }
+  }
+
+  export class MultiTypeMessage extends Message{
+    public msgClasses: Array<string>;
+
+    constructor(msgClasses: Array<string>){
+      super();
+      this.msgClasses = [];
+
+      msgClasses.forEach((msgClass) => {
+        this.msgClasses.push(msgClass);
+      });
+    }
+
+    public toPacketWithName(messageName: string){
+      const packet = { [messageName]:this };
+      return packet;
+    }
+
+    public fromPacketStr(packet:any): string{
+      var msgClass;
+
+      this.msgClasses.forEach((msgName) => {
+        if(msgName in packet){
+          msgClass = msgName;
+        }
+      });
+
+      if (!msgClass){ 
+        return ""; 
+      }
+
+      const msgData = packet[msgClass];
+      
+      for (var msgField in this) {
+        if (! (msgField in msgData)){ continue; }
+        const varType = typeof msgData[msgField];
+        if (varType in ["function","undefined","symbol"] ){ continue; }
+
+        if (varType === "object") {
+          this[msgField] = Object.assign(msgData[msgField]);
+        } else {
+          this[msgField] = msgData[msgField];
+        }
+      }
+
+      return msgClass;
     }
   }
 
@@ -315,9 +467,13 @@ export namespace Packets{
   // Requests ---------------------------------
   export namespace Request{
     export class Message extends Packets.Message {}
-    export class Handshake extends Message {
+    export class Handshake extends MultiTypeMessage {
       public version: number = 1;
       public magic: string = "coco";
+
+      constructor(){
+        super(['Handshake']);
+      }
     }
     export class GameList extends Message  {}
     export class GameDescription extends Message  {
@@ -369,8 +525,12 @@ export namespace Packets{
 
   export namespace Reply{
     export class Message extends Packets.Message {}
-    export class Handshake extends Message {
+    export class Handshake extends MultiTypeMessage {
       public magic: string = "";
+
+      constructor(){
+        super(['Handshake']);
+      }
     }
     export class GameList extends Message { 
       public games= new Array<string>() 
@@ -378,22 +538,27 @@ export namespace Packets{
     export class GameDescription extends Message { 
       public  description: string = "" 
     }
-    export class GameNew extends Message { 
+    export class GameNew extends Message {
       public id = {"Ok": "", "Err": ""}
     }
-    export class LobbyList extends Message { 
-      public info= new Array<MatchInfo>() 
+
+    export class LobbyList extends Message {
+      public info= new Array<MatchInfo>()
     }
+
     export class LobbySubscribed extends Message { 
       public seed = Array<MatchInfo>() 
     }
-    export class LobbyJoinedMatch extends Message { 
+
+    export class ConnectReply extends MultiTypeMessage {}
+
+    export class LobbyJoinedMatch extends ConnectReply { 
       public info = new Result<MatchInfo,string>() 
     }
     export class LobbyNew extends Message { 
       public info = new MatchInfo() 
     }
-    export class LobbyUpdate extends Message {
+    export class LobbyUpdate extends ConnectReply {
       public info = new MatchInfo()
     }
     export class LobbyDelete extends Message { 
