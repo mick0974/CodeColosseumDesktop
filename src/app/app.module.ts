a12c409dd09f07ad8cd604cc16b13dc30a665d03
SimoneBersani <sbersani.info@gmail.com>
2022-11-29 11:19:04 +0100

    [ADD] Configurazione componenti base e routeing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,37 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { routes } from './routes';
-
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { ConnectViewComponent } from './views/connect-view/connect-view.component';
+import { GameViewComponent } from './views/game-view/game-view.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { DropdownModule } from 'primeng/dropdown';
+import { StepsModule } from 'primeng/steps';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    ConnectViewComponent,
+    GameViewComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     FormsModule,
+    RouterModule.forRoot(routes),
+    InputTextModule,
+    InputSwitchModule,
+    DropdownModule,
+    StepsModule,
+    FileUploadModule,
+    ConfirmPopupModule,
+    TooltipModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
 
 @Component({
-  selector: 'tal-home-view',
+  selector: 'app-home-view',
   templateUrl: './home-view.component.html',
   styleUrls: ['./home-view.component.scss']
 })
@@ -11,10 +10,6 @@ export class HomeViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    let api = new ApiService();
-    api.gameList((gameList)=>{
-      alert(gameList);
-    });
   }
 
 }
