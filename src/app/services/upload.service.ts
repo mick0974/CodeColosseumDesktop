e62969b2ed5e4e6e26485970a77bf9fdda98c644
Fabs <fabretti.fabiola@gmail.com>
2022-12-04 12:10:54 +0100

    Reingeneered game views
    Before, the navigation was done through URL parameters (es. game/gameid/upload). This was kinda bad because it allowed someone to navigate improperly through the steps by manipulating the URL. Now it's done internally through a service. (probably it's still not optimal but this is what I came up with)

---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Game } from '../Game';
+import { GAMES } from 'mock-games';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  game! : Game | null;
+  gameId:string="";
+
+  constructor() { }
+
+  setGame(token:string|null):string|null{
+      // TODO: This check has to be substituted with getting the game from the API
+
+    if (token){
+
+      this.gameId=token;
+
+
+      //Check: does the game exist? If so, save it
+      GAMES.find( (g) => (this.game = g.id === this.gameId ? g : null) )
+
+      if(this.game){
+        console.log("[Service] Game set to "+ this.gameId)
+        console.log(this.game)
+      }
+      else{
+        console.log("[Service] Attempted to edit game that doesn't exist!")
+      }
+
+
+      return this.gameId
+    }
+    else {
+      // No ID was given: redirect
+      console.log("[Service] No ID given; redirect")
+      return (null)
+    }
+  }
+
+  getGameId(){
+    return this.gameId
+  }
+
+  isGameSet(){
+    return this.game !== null
+  }
+  reset(){
+    this.game=null;
+    this.gameId=""
+  }
+}
--- src/app/views/game-view/game-view.component.ts
+++ src/app/views/game-view/game-view.component.ts
@@ -3,6 +3,8 @@ import { Game } from 'src/app/Game';
 import { GAMES } from 'mock-games';
 import { ActivatedRoute } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { UploadService } from 'src/app/services/upload.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-view',
@@ -11,7 +13,7 @@ import { MenuItem } from 'primeng/api';
 })
 export class GameViewComponent implements OnInit {
   gameExists : boolean = false;
-  gameId : string="" ;
+  gameId : string= this.uploadService.getGameId() ;
   game! : Game | null;
   currStep : number = 0;
   
@@ -22,17 +24,28 @@ export class GameViewComponent implements OnInit {
 ];
 
 
-  constructor(private activatedroute:ActivatedRoute) { }
+  constructor(private router:Router,private activatedroute:ActivatedRoute, private uploadService:UploadService) { }
 
   ngOnInit(): void {
 
     //TODO: redirect to a 404 page rather than displaying the message here
+    
     let token = this.activatedroute.snapshot.paramMap.get('id');
 
     if (token){
-      this.gameId=token;
-      // This has to be substituted with getting the game from the API
-      GAMES.find( (g) => (this.game = g.id === this.gameId ? g : null) )
+      this.uploadService.setGame(token);
+      console.log("[Gameview] Setting game! Redirecting...")
+      this.router.navigate(['game'])
+  }
+    else{
+      if (this.uploadService.isGameSet()){
+        console.log("[Gameview] Game was already set to "+this.uploadService.getGameId())
+        this.router.navigate(['game/upload'])
+      }
+      else{
+        console.log("[Gameview] Game was not set! ")
+        //this.router.navigateByUrl("/home")
+      }
     }
 
     
