ee54038da43a6f6eaec4d5ca38bd499e989d2c3d
Cesare Montresor <cesare.montresor@gmail.com>
2022-12-05 19:50:10 +0100

    - typo

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { DemoViewComponent } from './views/demo-view/demo-view.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/demo', pathMatch: 'full' },
-    { path: 'demo', loadChildren: () => import('./views/demo-view/demo-view/demo-view.component').then(m => m.HomeModule) },
+    { path: 'demo', loadChildren: () => import('./views/demo-view/demo-view.component').then(m => m.DemoViewComponent) },
 ];
