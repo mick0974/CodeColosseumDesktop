7897160fd2a09c7e9ae3e986628b64c6a04a1b5d
Cesare Montresor <cesare.montresor@gmail.com>
2022-12-05 20:16:57 +0100

    API + Tauri

--- src/app/views/demo-view/demo-view.component.ts
+++ src/app/views/demo-view/demo-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone, OnInit } from '@angular/core';
 import { Child, Command, open as ShellOpen } from '@tauri-apps/api/shell'
 import { open as DialogOpen } from '@tauri-apps/api/dialog';
+import { ApiService } from 'src/app/services/api-service/api.service';
 @Component({
   selector: 'app-demo-view',
   templateUrl: './demo-view.component.html',
@@ -12,11 +13,65 @@ export class DemoViewComponent implements OnInit {
   public output = "";
 
   constructor(
-    public zone: NgZone
+    public zone: NgZone,
+    private api: ApiService
     ) { }
 
   ngOnInit(): void {
   }
+
+
+  async actionSpectate(){
+    alert("API is available");
+    let type = "spectate";
+
+    this.api.gameList((gameList)=>{
+      console.log(gameList);
+    });
+
+    if(type === "play") {
+      this.api.createNewGame((gameNew) => {
+        console.log('New game created: ' + gameNew);
+        this.api.connectToPlay(
+          (lobbyData) => console.log(lobbyData), 
+          (lobbyUpdated) => console.log(lobbyUpdated),
+          (matchStarted) => console.log(matchStarted),
+          (binaryInfo) => console.log(binaryInfo),
+          (matchEnded) => console.log(matchEnded),
+          gameNew, "Lollo123", undefined, 
+          (error) => alert(error));
+      }, "Lobby", "roshambo", 2, 1, undefined, undefined, undefined, undefined, (error) => {
+        alert("error 3");
+      });
+  
+      (new Promise(resolve => setTimeout(resolve, 5000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 6000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 7000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 8000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 9000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 10000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 11000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 12000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 13000))).then(() => this.api.play("ROCK\n"));
+      (new Promise(resolve => setTimeout(resolve, 14000))).then(() => this.api.play("ROCK\n"));
+  
+    } else {
+      this.api.connectToSpectate(
+        (spectateJoined) => console.log(spectateJoined), 
+        (spectateStarted) => console.log(spectateStarted),
+        (spectatSynced) => console.log(spectatSynced),
+        (lobbyUpdated) => console.log(lobbyUpdated),
+        (binaryMessage) => console.log(binaryMessage),
+        (spectateEnded) => console.log(spectateEnded),
+        "3m41h0rn9meb4"
+      ); 
+    }
+
+  }
+
+
+
+
 
 
   async actionRock() {
