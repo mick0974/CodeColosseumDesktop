3416ccdc9367e1a578bfa8c5b4ddb05e6ce2c73b
Cesare Montresor <cesare.montresor@gmail.com>
2022-12-05 17:17:38 +0100

    temp: added back exec demo

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,3 +1,5 @@
+import { Child, Command, open as ShellOpen } from '@tauri-apps/api/shell'
+import { open as DialogOpen } from '@tauri-apps/api/dialog';
 import { Component, OnInit, NgZone } from '@angular/core';
 import { GAMES } from 'mock-games';
 import { Game } from 'src/app/Game';
@@ -14,6 +16,11 @@ export class HomeViewComponent implements OnInit {
   loading: boolean = true;
   stateOptions: any[] = [{ icon: 'pi pi-bars', value: 'table' }, { icon: 'pi pi-th-large', value: 'card' }];
   view_mode: string = "table";
+  filename = "";
+
+  //Tauri dev
+  output = "";
+  child: Child | undefined;
 
   constructor(
     public zone: NgZone
@@ -26,6 +33,77 @@ export class HomeViewComponent implements OnInit {
       this.loading = false;
     }, 1000);
   }
+
+
+  async actionRock() {
+    await this.child?.write("ROCK\n");
+    this.output += `IN: ROCK\n`;
+    console.log(`IN: ROCK\n`);
+    this.refreshOutput();
+  }
+  async actionPaper() {
+    await this.child?.write("PAPER\n");
+    this.output += `IN: PAPER\n`;
+    console.log(`IN: PAPER\n`);
+    this.refreshOutput();
+  }
+  async actionScissor() {
+    await this.child?.write("SCISSOR\n");
+    this.output += `IN: SCISSOR\n`;
+    console.log(`IN: SCISSOR\n`);
+    this.refreshOutput();
+  }
+
+  public refreshOutput() {
+    this.zone.run(() => this.output += "")
+  }
+
+
+
+  async actionExec() {
+    console.log("EXEC!")
+    this.output = "";
+    const command = new Command("sh", ["-c", `${this.filename} 3 1`]);
+    command.stdout.on("data", (line: any) => {
+      this.output += `OUT: ${line}\n`;
+      console.log(`OUT: ${line}`);
+      this.refreshOutput();
+    });
+    command.stderr.on("data", (line: any) => {
+      this.output += `ERR: ${line}\n`;
+      console.log(`ERR: ${line}`);
+      this.refreshOutput();
+    });
+    this.child = await command.spawn();
+  }
+
+  async actionCompile() {
+    console.log("COMPILE!")
+    this.output = "";
+    const command = new Command("sh", ["-c", `${this.filename}.sh`])
+    command.stdout.on("data", (line: any) => {
+      this.output += `OUT: ${line}\n`;
+      console.log(`OUT: ${line}`);
+      this.refreshOutput();
+    });
+    command.stderr.on("data", (line: any) => {
+      this.output += `ERR: ${line}\n`;
+      console.log(`ERR: ${line}`);
+      this.refreshOutput();
+    });
+    this.child = await command.spawn();
+  }
+
+  async actionSearch() {
+    console.log("SEARCH!")
+    this.filename = <string>await DialogOpen({
+      multiple: false,
+    });
+    console.log(this.filename);
+  }
+
+
+
 }
 
 
