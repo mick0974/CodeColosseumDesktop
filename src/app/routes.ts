db7a47e05559e4d52c918c9677adb9a5d34104b9
Cesare Montresor <cesare.montresor@gmail.com>
2022-12-05 20:04:54 +0100

    module to component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,5 +3,5 @@ import { DemoViewComponent } from './views/demo-view/demo-view.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/demo', pathMatch: 'full' },
-    { path: 'demo', loadChildren: () => import('./views/demo-view/demo-view.component').then(m => m.DemoViewComponent) },
+    { path: 'demo', component: DemoViewComponent },
 ];
