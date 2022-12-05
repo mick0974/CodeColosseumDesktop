2d8368e8221d4fe52ea0e666d7c7c83a6b70d00d
Cesare Montresor <cesare.montresor@gmail.com>
2022-12-05 19:20:48 +0100

    - minor

--- src/app/services/api-service/api.socket.ts
+++ src/app/services/api-service/api.socket.ts
@@ -2,59 +2,6 @@ import { WebSocketSubject } from 'rxjs/webSocket';
 import { Packets } from './api.packets';
 
 export namespace CoCoSockets{
-  /*
-  export class CoCoSocket{
-    public url: string;
-    public ws?:WebSocketSubject<string>;
-    public resultError?:(error:string)=>void;
-    public resultClosed?:()=>void;
-    
-    constructor(url:string){
-      this.url = url;
-    }
-  
-    public connect():boolean{
-  
-      if (!this.ws || this.ws.closed ){
-        this.ws = new WebSocketSubject(this.url);
-      }
-      
-      this.ws.subscribe({
-        error: (err) => { // Called whenever there is a message from the server.
-          let errorMsg = JSON.stringify(err);
-          if (this.resultError) {this.resultError(errorMsg );}
-        },
-        complete: () => {
-          if (this.resultClosed) {this.resultClosed();}
-        }
-      });
-      
-      return !this.ws.closed;
-    }
-    
-    public send<T extends Packets.Reply.Message>(
-      request:Packets.Request.Message, 
-      reply: new ()=>T, 
-      recieved?:(message:T)=>void){
-      
-      if (this.ws == null) {return false}
-      
-      this.ws.subscribe({
-        next: (payload) => { // Called whenever there is a message from the server.
-          let msg = new reply();
-          let success = msg.fromPacket(payload);
-          if (success && recieved){ recieved(msg); }
-        } 
-      });
-  
-  
-      let packet = request.toPacket();
-      this.ws.next(packet);
-  
-      return this.ws;
-    }
-  }
-  */
   
   export class CoCoSocket{
     public url = 'ws://localhost:8088';
@@ -134,4 +81,61 @@ export namespace CoCoSockets{
       return this.ws;
     }
   }
+
+
+
+
+   /*
+  export class CoCoSocket{
+    public url: string;
+    public ws?:WebSocketSubject<string>;
+    public resultError?:(error:string)=>void;
+    public resultClosed?:()=>void;
+    
+    constructor(url:string){
+      this.url = url;
+    }
+  
+    public connect():boolean{
+  
+      if (!this.ws || this.ws.closed ){
+        this.ws = new WebSocketSubject(this.url);
+      }
+      
+      this.ws.subscribe({
+        error: (err) => { // Called whenever there is a message from the server.
+          let errorMsg = JSON.stringify(err);
+          if (this.resultError) {this.resultError(errorMsg );}
+        },
+        complete: () => {
+          if (this.resultClosed) {this.resultClosed();}
+        }
+      });
+      
+      return !this.ws.closed;
+    }
+    
+    public send<T extends Packets.Reply.Message>(
+      request:Packets.Request.Message, 
+      reply: new ()=>T, 
+      recieved?:(message:T)=>void){
+      
+      if (this.ws == null) {return false}
+      
+      this.ws.subscribe({
+        next: (payload) => { // Called whenever there is a message from the server.
+          let msg = new reply();
+          let success = msg.fromPacket(payload);
+          if (success && recieved){ recieved(msg); }
+        } 
+      });
+  
+  
+      let packet = request.toPacket();
+      this.ws.next(packet);
+  
+      return this.ws;
+    }
+  }
+  */
 }
